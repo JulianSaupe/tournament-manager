c8a09910380a7fbd9c8534c53be379bdcbe6f486
Julian Saupe <julian@saupe-medien.de>
2026-01-13 20:09:47 +0100

    Migrate Protobuf files to shared directory and update `build.rs` paths.

--- backend/identity-service/build.rs
+++ backend/identity-service/build.rs
@@ -1,14 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure().compile_protos(
         &[
-            "proto/authentication.proto",
-            "proto/user.proto",
-            "proto/authorization/authorization.proto",
-            "proto/authorization/permission.proto",
-            "proto/authorization/role.proto",
-            "proto/authorization/user_role.proto",
+            "../shared/proto/authentication.proto",
+            "../shared/proto/user.proto",
+            "../shared/proto/authorization/authorization.proto",
+            "../shared/proto/authorization/permission.proto",
+            "../shared/proto/authorization/role.proto",
+            "../shared/proto/authorization/user_role.proto",
         ],
-        &["proto"],
+        &["../shared/proto"],
     )?;
     Ok(())
 }
